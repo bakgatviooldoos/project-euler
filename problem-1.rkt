190f20d3fb834019e14acdeb32227ab5673c5acb
bakgatviooldoos <56836858+bakgatviooldoos@users.noreply.github.com>
2024-03-09 11:20:46 +0200

    cleaning up, added a test to clarify the intent

--- problem-1.rkt
+++ problem-1.rkt
@@ -1,6 +1,7 @@
 #lang racket
 
-(require math/number-theory)
+(require math/number-theory
+         rackunit)
 
 #|
 Multiples of 3 or 5
@@ -12,19 +13,21 @@ The sum of these multiples is 23.
 Find the sum of all the multiples of or below 1000.
 |#
 
-; brute-force
-(define (find-sum-of-multiples below . factors)
+; naive brute-force: guaranteed to be correct, terrible runtime
+(define (find-sum-of-multiples:naive below . factors)
   (define (count? n)
     (ormap (lambda (factor) (divides? factor n)) factors))
-  (for/sum ([n (in-range 1 below)] #:when (count? n)) n))
+  (for/sum ([n (in-range (apply min factors) below)] #:when (count? n))
+    n))
 
-; number-theoretic
-(define (find-sum-of-multiples* below . factors)
+; number-theoretic: better runtime, more complicated to explain
+(define (find-sum-of-multiples:better below . factors)
   (define products-and-ωs
     ; we calculate all of the combinations of the factors, i.e.
     ; for factors=(3,5,7), we have 3,5,7,(3,5),(3,7),(5,7),(3,5,7)
+    ; and then multiply the factors of each combination
     (for/list ([prod (in-combinations factors)]
-               ; we discard the empty product
+               ; we discard the empty product, i.e. 1
                #:unless (empty? prod))
       ; we also keep track of how *many* factors are in a combination
       ; because we use this for the inclusion/exclusion principle
@@ -33,9 +36,9 @@ Find the sum of all the multiples of or below 1000.
   (define bounds-per-product
     ; we calculate the "bounds" of the products above, which count the total number
     ; of multiples of each of the products below our upper-bound
-    (map (compose (curry quotient (- below 1))
-                  first)
-         products-and-ωs))
+    (let ([the-product first])
+      (map (compose (curry quotient (- below 1)) the-product)
+           products-and-ωs)))
   
   (for/sum ([product/ω products-and-ωs]
             [bound     bounds-per-product])
@@ -55,7 +58,8 @@ Find the sum of all the multiples of or below 1000.
        ; i.e triangular number
        (triangle-number bound))))
 
+; > (find-sum-of-multiples:naive 1000 3 5)
 ; 233168
-(list 1000
-      (find-sum-of-multiples  1000 3 5)
-      (find-sum-of-multiples* 1000 3 5))
+
+(check-equal? (find-sum-of-multiples:naive  1000 3 5)
+              (find-sum-of-multiples:better 1000 3 5))
